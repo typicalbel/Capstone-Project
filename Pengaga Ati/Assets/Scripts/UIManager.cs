b97c20270226d667799827d387a1c8d85c1bfc3a
typicalbel <shinyeechin@hotmail.com>
2021-10-11 17:02:39 +0800

    script test

--- Pengaga Ati/Assets/Scripts/UIManager.cs
+++ Pengaga Ati/Assets/Scripts/UIManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//test test
 public class UIManager : MonoBehaviour
 {
     public void playGame()
